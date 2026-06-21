e2a79e14555da34f225fbfb5fce72cb5fcebe44d
ayush <ayushbhatnagar71@gmail.com>
2026-06-21 14:08:38 +0530

    Completed Day_21

---
+++ Day_21/Question_81.cpp
@@ -0,0 +1,17 @@
+//Write a program to Find string length without strlen().
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    int length=0;
+
+    cout<<"Enter the string: ";
+    getline(cin, str);
+    for (int i=0;str[i]!='\0';i++){
+        length++;
+    }
+    cout<<"Length is: "<<length<<endl;
+    return 0;
+}
---
+++ Day_21/Question_84.cpp
@@ -0,0 +1,17 @@
+//Write a program to Convert lowercase to uppercase.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str;
+    cout<<"Enter the string: ";
+    getline(cin,str);
+    for(int i=0;i<str.length();i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i]=str[i]-32;
+        }
+    }
+    cout<<"Uppercase string is: "<<str<<endl;
+    return 0;
+}

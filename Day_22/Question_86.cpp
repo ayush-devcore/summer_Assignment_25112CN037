f2fcc85ccc0bb62e36d1e164ee9bf909d1d3dcd1
ayush <ayushbhatnagar71@gmail.com>
2026-06-22 23:49:31 +0530

    Completed Day_22

---
+++ Day_22/Question_86.cpp
@@ -0,0 +1,20 @@
+//Write a program to Count words in a sentence.
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string s;
+    cout<<"Enter a sentence: ";
+    getline(cin, s);
+    int totalWords=0;
+    for (int i=0;i<s.length();i++) {
+        if (s[i]!=' ') {
+            if (i==0||s[i-1]==' ') {
+                totalWords++;
+            }
+        }
+    }
+    cout<<"Total number of words: "<<totalWords<< endl;
+    return 0;
+}
